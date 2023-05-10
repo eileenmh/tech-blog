64d0bd7b85812f46ea624888898c83e452f527b2
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-05-10 00:51:58 -0400

    Set up render for homepage

--- server.js
+++ server.js
@@ -6,3 +6,9 @@ import { engine } from "express-handlebars";
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000);
