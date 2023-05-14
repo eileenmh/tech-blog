e55dc726717938b689f399f21422d757636c451a
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-05-13 22:28:39 -0400

    add associations betweens models

--- models/index.js
+++ models/index.js
@@ -2,4 +2,13 @@ const User = require("./User");
 const Article = require("./Article");
 const Comment = require("./Comment");
 
+User.hasMany(Article);
+Article.belongsTo(User);
+
+User.hasMany(Comment);
+Comment.belongsTo(User);
+
+Article.hasMany(Comment);
+Comment.belongsTo(Article);
+
 module.exports = { User, Article, Comment };
