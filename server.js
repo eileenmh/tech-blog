91a6c7c5f71586127c16453adad5b8d44aa34d79
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-05-18 21:57:11 -0400

    create session object

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const path = require("path");
 const sequelize = require("./config/connection");
 const express = require("express");
+
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sess = {
+  secret: "",
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// const models = require("./models");
 
 const { engine } = require("express-handlebars");
 const routes = require("./controllers");
