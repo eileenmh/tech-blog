1d77aa07cc2eac4a96c2ce3b975d0dbdcb6ca37a
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-05-25 22:55:50 -0400

    use bcrypt to hash passwords

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 const { User } = require("../../models");
 
 router.post("/username", async (req, res) => {
@@ -19,10 +20,9 @@ router.post("/username", async (req, res) => {
 
 router.post("/signup", async (req, res) => {
   try {
-    userData = await User.create({
-      username: req.body.username,
-      password: req.body.password,
-    });
+    const newUser = req.body;
+    newUser.password = await bcrypt.hash(req.body.password, 10);
+    userData = await User.create(newUser);
 
     req.session.save(() => {
       req.session.user_id = userData.id;
@@ -38,13 +38,23 @@ router.post("/signup", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const userData = await User.findOne({
-      where: { username: req.body.username, password: req.body.password },
+      where: { username: req.body.username },
     });
 
     if (!userData) {
       res
         .status(400)
         .json({ message: "Incorrect email or password, please try again." });
+      return;
+    }
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      userData.password
+    );
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Login failed. Please try again!" });
       return;
     }
 
