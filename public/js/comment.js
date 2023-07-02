367f66df094848c6f63bc457b918fc00ab1d76d0
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-07-01 21:25:48 -0400

    add feature to delete a post

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,11 +1,18 @@
-// const editPost = async (event) => {
-//   event.preventDefault();
-//   console.log("Edit post button clicked");
-//   const articleId = $(event.target).parents("#article").attr("data-id");
+const deletePost = async (event) => {
+  const id = $("#article").attr("data-id");
 
-//   const response = await fetch("/api/article/e")
-//   console.log(articleId);
-// };
+  const response = await fetch("/api/article/delete", {
+    method: "DELETE",
+    body: JSON.stringify({ id }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace(`/dashboard`);
+  } else {
+    console.log("No luck.");
+  }
+};
 
 const createComment = async (event) => {
   event.preventDefault();
@@ -30,4 +37,5 @@ const createComment = async (event) => {
 $(document).ready(function () {
   $("#createComment").on("click", createComment);
   $("#editPost").on("click", editPost);
+  $("#deletePost").on("click", deletePost);
 });
